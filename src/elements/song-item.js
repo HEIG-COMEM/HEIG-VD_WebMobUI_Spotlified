8471a62d1cac55d84f8ae8250b4a5fd93618747d
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-03-11 15:56:06 +0100

    Changing the favorite button icon in SongItem

--- src/elements/song-item.js
+++ src/elements/song-item.js
@@ -6,8 +6,8 @@ class SongItem extends HTMLElement {
     this.innerHTML = `<a href="#">
     <div class="list-item-title">${this.getAttribute('title')}</div>
     <div class="list-item-actions">
-      <button type="button" class="icon-button favorite-button ">
-        <span class="material-icons">playlist_add</span>
+      <button type="button" class="icon-button favorite-button">
+        <span class="material-icons">favorite</span>
       </button>
       <button type="button" class="icon-button play-button">
         <span class="material-icons">play_arrow</span>
